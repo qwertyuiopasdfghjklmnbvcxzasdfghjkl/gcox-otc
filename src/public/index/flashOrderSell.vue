42aaa1c32d4a6cba65e8ffb8f24320e8980b64ac
yangl <574748221@qq.com>
2019-12-05 17:36:01 +0800

    先登录才可以快捷下单

--- src/public/index/flashOrderSell.vue
+++ src/public/index/flashOrderSell.vue
@@ -37,7 +37,7 @@
       }
     },
     computed: {
-      ...mapGetters(['getLang', 'getSymbol', 'getCurrency', 'getUserInfo', 'getApiToken']),
+      ...mapGetters(['getLang', 'getSymbol', 'getCurrency', 'getUserInfo', 'getApiToken', 'isLogin']),
       price () {
         if (!this.amount) {
           return this.sellCur
@@ -76,7 +76,18 @@
       timeout (e) {
         console.log('timeout' + e)
         if (e) {
-          this.getAdv()
+          if (this.isLogin) {
+            this.getAdv()
+          } else {
+            Vue.$confirmDialog({
+              id: 'login',
+              showCancel: false,
+              content: this.$t('public0.public15'), // 请登录后再交易
+              okCallback: () => {
+                this.$router.push({name: 'login'})
+              }
+            })
+          }
         }
       },
       params () {
